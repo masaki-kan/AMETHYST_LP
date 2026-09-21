07e7deb34f00f7416db74ad50d134a190ef60a22
木村昌樹 <kimuramasaki@kimuramasakinoMacBook-Pro.local>
2026-09-21 10:11:13 +0900

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/AMETHYST_LP/',
+export default defineConfig(({ command }) => {
+  const base = command === 'serve' ? '/' : '/AMETHYST_LP/';
+
+  return {
+    plugins: [react()],
+    base,
+  };
 })
